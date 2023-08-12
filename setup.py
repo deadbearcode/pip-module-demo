445154c6d0c0a17fff8ab4f58625bcab17891ba7
Sean Baier <sean.t.baier@gmail.com>
2023-08-12 12:46:39 -0500

    update desc and add module

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
 from setuptools import find_packages, setup
 
+with open("README.md", "r") as file:
+    long_description = file.read()
+
 setup(
     name="deadbear_fib_py",
     version="0.0.1",
     author="Sean Baier",
     author_email="sean.baier@deadbear.io",
     description="Calculates a Fibonacci number",
-    long_description="A basic library that calculates Fibonacci numbers",
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/deadbearcode/pip-module-demo",
     install_requires=[],
