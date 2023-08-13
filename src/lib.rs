d6e9583017601d7834407462a76ea2bfc06b17c0
Sean Baier <sean.t.baier@gmail.com>
2023-08-13 09:49:44 -0500

    adding rust pyfunctions

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+mod fib_calcs;
+
+use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
+use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
+pub mod fib_numbers;
+
 #[pyfunction]
 fn say_hello() {
     println!("saying hello from Rust!");
@@ -9,5 +15,7 @@ fn say_hello() {
 #[pymodule]
 fn deadbear_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(say_hello));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number));
+    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     Ok(())
 }
