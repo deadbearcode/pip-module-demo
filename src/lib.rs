f8ab712fa2141fc3304d094346cfc0f65ce653f0
Sean Baier <sean.t.baier@gmail.com>
2023-08-13 16:21:05 -0500

    create Python in class in Rust

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
+mod class_module;
 mod fib_calcs;
 mod interface;
 
+use class_module::fib_processor::FibProcessor;
 use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
 use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
 use interface::config::__pyo3_get_function_run_config;
@@ -21,5 +23,6 @@ fn deadbear_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
     m.add_wrapped(wrap_pyfunction!(run_config));
     m.add_wrapped(wrap_pyfunction!(object_interface));
+    m.add_class::<FibProcessor>()?;
     Ok(())
 }
