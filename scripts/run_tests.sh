0f24a88bf69b6835cae63fd5bdf9e07a2b92a238
Sean Baier <sean.t.baier@gmail.com>
2023-08-13 09:09:12 -0500

    refactor to rust

--- scripts/run_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
-
-source .venv/bin/activate
-cd $SCRIPTPATH
-cd ..
-export PYTHONPATH="./deadbear_fib_py"
-python -m unittest discover
